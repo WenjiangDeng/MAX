b12612eabeddca8c99c8829380ef8aced565c7dd
WenjiangDeng <40486459+WenjiangDeng@users.noreply.github.com>
2022-02-04 16:03:44 +0800

    Update APE-source.R

--- Scripts_and_Files/Accuracy-Evaluation/APE-source.R
+++ Scripts_and_Files/Accuracy-Evaluation/APE-source.R
@@ -1,4 +1,3 @@
-
 	
 err = function(result_est,true){##salmon and true are expanded
 
@@ -14,12 +13,12 @@ err = function(result_est,true){##salmon and true are expanded
 }
 
 
-expand = function(result_est,Sailfish)
-{##result_est from sequgio , Sailfish from different methods
-sailfish1 = matrix(0,length(unique(unlist(strsplit(rownames(result_est)," ")))),100)
-rownames(sailfish1) = unique(unlist(strsplit(rownames(result_est)," ")))
-colnames(sailfish1) = colnames(result_est)
-sailfish1[rownames(sailfish1),] = Sailfish[rownames(sailfish1),]
+expand = function(result_est,salmon)
+{##result_est from sequgio , salmon from different methods
+salmon1 = matrix(0,length(unique(unlist(strsplit(rownames(result_est)," ")))),100)
+rownames(salmon1) = unique(unlist(strsplit(rownames(result_est)," ")))
+colnames(salmon1) = colnames(result_est)
+salmon1[rownames(salmon1),] = salmon[rownames(salmon1),]
 
 salmon = matrix(0,nrow(result_est),100)
 rownames(salmon) = rownames(result_est)
@@ -27,12 +26,12 @@ colnames(salmon) = colnames(result_est)
 rowname = rownames(result_est)
 tx = strsplit(rownames(result_est)," ")
 ntx = sapply(tx,length)
-rowname_sailfish = rownames(Sailfish)[ rownames(Sailfish) %in% rowname]
-salmon[rowname_sailfish,] = Sailfish[rowname_sailfish,]
+rowname_salmon = rownames(salmon)[ rownames(salmon) %in% rowname]
+salmon[rowname_salmon,] = salmon[rowname_salmon,]
 pick = ntx>1
 tx2 = tx[pick]
 for(i in 1:length(tx2)){
- tmp = sailfish1[tx2[[i]],];tmp2 = colSums(tmp);salmon[paste(tx2[[i]],collapse=" "),] = tmp2;
+ tmp = salmon1[tx2[[i]],];tmp2 = colSums(tmp);salmon[paste(tx2[[i]],collapse=" "),] = tmp2;
  			}
 ## salmon 
 return(salmon)

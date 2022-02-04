ed038640bd1fb4c8bd08ae0768d7598d0111a489
WenjiangDeng <40486459+WenjiangDeng@users.noreply.github.com>
2022-02-04 13:02:34 +0800

    Update APE-source.R

--- Scripts_and_Files/Accuracy-Evaluation/APE-source.R
+++ Scripts_and_Files/Accuracy-Evaluation/APE-source.R
@@ -6,15 +6,16 @@ err = function(result_est,true){##salmon and true are expanded
 	rownames(seq_err) = rownames(result_est)
 
 	for(i in 1:nrow(result_est))
-{
+	{
 	seq_err2 = abs(result_est[i,] - true[i,])/(true[i,]+1)
 	seq_err[i] = median(seq_err2,na.rm=TRUE)
-}
+	}
 	return(seq_err)
 }
 
 
-expand = function(result_est,Sailfish){##result_est from sequgio , Sailfish from different methods
+expand = function(result_est,Sailfish)
+{##result_est from sequgio , Sailfish from different methods
 sailfish1 = matrix(0,length(unique(unlist(strsplit(rownames(result_est)," ")))),100)
 rownames(sailfish1) = unique(unlist(strsplit(rownames(result_est)," ")))
 colnames(sailfish1) = colnames(result_est)
@@ -32,7 +33,7 @@ pick = ntx>1
 tx2 = tx[pick]
 for(i in 1:length(tx2)){
  tmp = sailfish1[tx2[[i]],];tmp2 = colSums(tmp);salmon[paste(tx2[[i]],collapse=" "),] = tmp2;
- }
+ 			}
 ## salmon 
 return(salmon)
 }
